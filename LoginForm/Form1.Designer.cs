dbb10c1d8936d6b9198fd6d71952d170dffb58e7
Timothy Aditya Sutantyo <timmychelseaFCbarca@hotmail.com>
2024-04-15 10:27:48 +0200

    Change button name to cancel

--- LoginForm/Form1.Designer.cs
+++ LoginForm/Form1.Designer.cs
@@ -40,9 +40,10 @@
             // confirmButton
             // 
             confirmButton.BackColor = SystemColors.ButtonFace;
-            confirmButton.Location = new Point(700, 371);
+            confirmButton.Location = new Point(490, 223);
+            confirmButton.Margin = new Padding(2, 2, 2, 2);
             confirmButton.Name = "confirmButton";
-            confirmButton.Size = new Size(149, 67);
+            confirmButton.Size = new Size(104, 40);
             confirmButton.TabIndex = 0;
             confirmButton.Text = "Confirm";
             confirmButton.UseVisualStyleBackColor = false;
@@ -51,20 +52,22 @@
             // cancelButton
             // 
             cancelButton.BackColor = SystemColors.ButtonFace;
-            cancelButton.Location = new Point(526, 371);
+            cancelButton.Location = new Point(368, 223);
+            cancelButton.Margin = new Padding(2, 2, 2, 2);
             cancelButton.Name = "cancelButton";
-            cancelButton.Size = new Size(149, 67);
+            cancelButton.Size = new Size(104, 40);
             cancelButton.TabIndex = 1;
-            cancelButton.Text = "Confirm";
+            cancelButton.Text = "Cancel";
             cancelButton.UseVisualStyleBackColor = false;
             cancelButton.Click += button2_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(36, 54);
+            label1.Location = new Point(25, 32);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(91, 25);
+            label1.Size = new Size(60, 15);
             label1.TabIndex = 2;
             label1.Text = "Username";
             label1.Click += label1_Click;
@@ -72,38 +75,42 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(36, 143);
+            label2.Location = new Point(25, 86);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(87, 25);
+            label2.Size = new Size(57, 15);
             label2.TabIndex = 3;
             label2.Text = "Password";
             label2.Click += label2_Click;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(36, 91);
+            textBox1.Location = new Point(25, 55);
+            textBox1.Margin = new Padding(2, 2, 2, 2);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(263, 31);
+            textBox1.Size = new Size(185, 23);
             textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(36, 186);
+            textBox2.Location = new Point(25, 112);
+            textBox2.Margin = new Padding(2, 2, 2, 2);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(263, 31);
+            textBox2.Size = new Size(185, 23);
             textBox2.TabIndex = 5;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(861, 450);
+            ClientSize = new Size(603, 270);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(cancelButton);
             Controls.Add(confirmButton);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Login Form";
             ResumeLayout(false);
